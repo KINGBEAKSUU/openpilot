d830e8b9994cf24ffb43d851e53058c8cb266099
kans <kandrea@nate.com>
2024-10-01 14:11:25 +0900

    for GM

--- common/params.cc
+++ common/params.cc
@@ -266,6 +266,8 @@ std::unordered_map<std::string, uint32_t> keys = {
     { "CarrotException", CLEAR_ON_MANAGER_START },
     { "CarName", CLEAR_ON_MANAGER_START },
     
+    {"EVTable", PERSISTENT},
+    {"LongPitch", PERSISTENT},    
 
 };
 
