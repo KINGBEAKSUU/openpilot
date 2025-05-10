1bdc25afab206e64f6876e7510e46b90020795c2
ajouatom <ajouatom@gmail.com>
2025-05-10 22:14:14 +0900

    fix..

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -89,7 +89,7 @@ procs = [
   NativeProcess("modeld", "selfdrive/modeld", ["./modeld"], only_onroad),
   NativeProcess("dmonitoringmodeld", "selfdrive/modeld", ["./dmonitoringmodeld"], enable_dm, enabled=(WEBCAM or not PC)),
   #NativeProcess("mapsd", "selfdrive/navd", ["./mapsd"], only_onroad),
-  NativeProcess("mapsd", "selfdrive/navd", ["./mapsd"], always_run),
+  #NativeProcess("mapsd", "selfdrive/navd", ["./mapsd"], always_run),
   PythonProcess("navmodeld", "selfdrive.modeld.navmodeld", only_onroad),
   NativeProcess("sensord", "system/sensord", ["./sensord"], only_onroad, enabled=not PC),
   NativeProcess("ui", "selfdrive/ui", ["./ui"], always_run, watchdog_max_dt=(5 if not PC else None)),
