fe92938551d658ca88e88d86ca3643d97f0acd16
ajouatom <ajouatom@gmail.com>
2025-05-10 15:18:35 +0900

    fix da3

--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -23,7 +23,7 @@ from openpilot.selfdrive.controls.lib.drive_helpers import get_accel_from_plan, 
 from openpilot.selfdrive.modeld.runners import ModelRunner, Runtime
 from openpilot.selfdrive.modeld.parse_model_outputs import Parser
 from openpilot.selfdrive.modeld.fill_model_msg import fill_model_msg, fill_pose_msg, PublishState
-from openpilot.selfdrive.modeld.constants import ModelConstants
+from openpilot.selfdrive.modeld.constants import ModelConstants, Plan
 from openpilot.selfdrive.modeld.models.commonmodel_pyx import ModelFrame, CLContext
 
 PROCESS_NAME = "selfdrive.modeld.modeld"
