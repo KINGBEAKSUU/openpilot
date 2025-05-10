b2cbde6bcbb5eee818073cdca1bb09163cef09ef
ajouatom <ajouatom@gmail.com>
2025-05-10 21:25:56 +0900

    ff

--- selfdrive/navd/main.cc
+++ selfdrive/navd/main.cc
@@ -50,6 +50,7 @@ int main(int argc, char *argv[]) {
   printf("##########main2\n");
   setenv("QT_QPA_PLATFORM", "xcb", 1);
   setenv("QT_QPA_PLATFORM", "minimal", 1);
+  setenv("QT_QPA_PLATFORM", "eglfs", 1);
 
   setenv("LIBGL_ALWAYS_SOFTWARE", "1", 1);
   setenv("QT_LOGGING_RULES", "qt.qpa.*=true;qt.opengl.*=true", 1);
