12ff3bfbf230ec941edcae958bf8b176cbcf752a
SangJo Kim <kandrea@nate.com>
2025-05-30 21:10:41 +0900

    fix

--- opendbc_repo/opendbc/car/gm/carstate.py
+++ opendbc_repo/opendbc/car/gm/carstate.py
@@ -120,7 +120,10 @@ class CarState(CarStateBase):
 
     ret.brake = pt_cp.vl["ECMAcceleratorPos"]["BrakePedalPos"]
     if self.CP.networkLocation == NetworkLocation.fwdCamera:
-      ret.brakePressed = pt_cp.vl["ECMEngineStatus"]["BrakePressed"] != 0
+      if self.CP.carFingerprint in (CAR.CHEVROLET_MALIBU_2019, CAR.CHEVROLET_EQUINOX):
+        ret.brakePressed = ret.brake >= 10
+      else:
+        ret.brakePressed = pt_cp.vl["ECMEngineStatus"]["BrakePressed"] != 0
     else:
       # Some Volt 2016-17 have loose brake pedal push rod retainers which causes the ECM to believe
       # that the brake is being intermittently pressed without user interaction.
