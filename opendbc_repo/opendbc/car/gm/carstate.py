40efcac6cdb30f3a747e24297665afab5babd69e
SangJo Kim <kandrea@nate.com>
2025-05-31 23:34:21 +0900

    fix

--- opendbc_repo/opendbc/car/gm/carstate.py
+++ opendbc_repo/opendbc/car/gm/carstate.py
@@ -107,18 +107,16 @@ class CarState(CarStateBase):
 
     if self.CP.flags & GMFlags.NO_ACCELERATOR_POS_MSG.value: # 190(0xBE)캔이 없으면 241(0xF1)을 사용하라.
       ret.brake = pt_cp.vl["EBCMBrakePedalPosition"]["BrakePedalPosition"] / 0xd0
-      ret.brakePressed = pt_cp.vl["EBCMBrakePedalPosition"]["BrakePedalPosition"] >= 15
     else:
       ret.brake = pt_cp.vl["ECMAcceleratorPos"]["BrakePedalPos"]
-      ret.brakePressed = pt_cp.vl["ECMAcceleratorPos"]["BrakePedalPos"] >= 10
     if self.CP.networkLocation == NetworkLocation.fwdCamera:
       ret.brakePressed = pt_cp.vl["ECMEngineStatus"]["BrakePressed"] != 0
-    #else:
+    else:
       # Some Volt 2016-17 have loose brake pedal push rod retainers which causes the ECM to believe
       # that the brake is being intermittently pressed without user interaction.
       # To avoid a cruise fault we need to use a conservative brake position threshold
       # https://static.nhtsa.gov/odi/tsbs/2017/MC-10137629-9999.pdf
-      #ret.brakePressed = ret.brake >= 10
+      ret.brakePressed = ret.brake >= 10
 
     # Regen braking is braking
     if self.CP.transmissionType == TransmissionType.direct:
