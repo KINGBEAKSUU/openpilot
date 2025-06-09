24f668931e5db4919ec68353ebcd5b94556e1a7b
kans <kandrea@nate.com>
2025-06-09 10:17:40 +0900

    fix

--- opendbc_repo/opendbc/safety/safety/safety_gm.h
+++ opendbc_repo/opendbc/safety/safety/safety_gm.h
@@ -195,7 +195,7 @@ static bool gm_tx_hook(const CANPacket_t *to_send) {
 
   // BUTTONS: used for resume spamming and cruise cancellation with stock longitudinal
 
-if ((addr == 0x1E1) && (gm_pcm_cruise || gm_pedal_long || gm_cc_long)) {
+  if ((addr == 0x1E1) && (gm_pcm_cruise || gm_pedal_long || gm_cc_long)) {
     int button = (GET_BYTE(to_send, 5) >> 4) & 0x7U;
 
     bool allowed_btn = (button == GM_BTN_CANCEL) && cruise_engaged_prev;
