29e5ca4e9c7b445d1fbca07cdfd0c80de9e0c505
SangJo Kim <kandrea@nate.com>
2025-06-09 22:46:05 +0900

    fix

--- opendbc_repo/opendbc/safety/safety/safety_gm.h
+++ opendbc_repo/opendbc/safety/safety/safety_gm.h
@@ -198,13 +198,18 @@ static bool gm_tx_hook(const CANPacket_t *to_send) {
   }
 
   // BUTTONS: used for resume spamming and cruise cancellation with stock longitudinal
-  if ((addr == 0x1E1) && (gm_pcm_cruise || gm_pedal_long || gm_cc_long)) {
+  if (addr == 0x1E1) {
     int button = (GET_BYTE(to_send, 5) >> 4) & 0x7U;
 
     bool allowed_btn = (button == GM_BTN_CANCEL) && cruise_engaged_prev;
-    // For standard CC, allow spamming of SET / RESUME
-    if (gm_cc_long) {
-      allowed_btn |= cruise_engaged_prev && ((button == GM_BTN_SET) || (button == GM_BTN_RESUME) || (button == GM_BTN_UNPRESS));
+
+    if (!gm_pcm_cruise &&  ((gm_hw == GM_ASCM) || gm_cam_long)) {
+      // OP 롱컨 + CAM_LONG 차량
+      allowed_btn |= ((button == GM_BTN_SET) || (button == GM_BTN_RESUME) || (button == GM_BTN_UNPRESS));
+    } else if (gm_pcm_cruise || gm_pedal_long || gm_cc_long) {
+      if (gm_cc_long) {
+        allowed_btn |= cruise_engaged_prev && ((button == GM_BTN_SET) || (button == GM_BTN_RESUME) || (button == GM_BTN_UNPRESS));
+      }
     }
 
     if (!allowed_btn) {
