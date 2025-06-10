ac4c14bbaec47bfb51ce6d29afcc75a89c846cbd
SangJo Kim <kandrea@nate.com>
2025-06-10 15:05:54 +0900

    test

--- opendbc_repo/opendbc/safety/safety/safety_gm.h
+++ opendbc_repo/opendbc/safety/safety/safety_gm.h
@@ -189,7 +189,7 @@ static bool gm_tx_hook(const CANPacket_t *to_send) {
 
     bool violation = false;
     // Allow apply bit in pre-enabled and overriding states
-    violation |= !controls_allowed && apply;
+    //violation |= !controls_allowed && apply;  //테스트: apply체크 제거
     violation |= longitudinal_gas_checks(gas_regen, *gm_long_limits);
 
     if (violation) {
