7186d62cb83436e257b4232566d4546e2a0de106
SangJo Kim <kandrea@nate.com>
2025-06-12 23:07:35 +0900

    re add

--- opendbc_repo/opendbc/car/gm/carcontroller.py
+++ opendbc_repo/opendbc/car/gm/carcontroller.py
@@ -295,6 +295,10 @@ class CarController(CarControllerBase):
           else:
             acc_engaged = CC.enabled
 
+          if actuators.longControlState in [LongCtrlState.stopping, LongCtrlState.starting]:
+            if (self.frame - self.last_button_frame) * DT_CTRL > 0.04:
+              self.last_button_frame = self.frame
+              can_sends.append(gmcan.create_buttons(self.packer_pt, CanBus.POWERTRAIN, (CS.buttons_counter + 1) % 4, CruiseButtons.RES_ACCEL))
           # GasRegenCmdActive needs to be 1 to avoid cruise faults. It describes the ACC state, not actuation
           can_sends.append(gmcan.create_gas_regen_command(self.packer_pt, CanBus.POWERTRAIN, self.apply_gas, idx, acc_engaged, at_full_stop))
           can_sends.append(gmcan.create_friction_brake_command(self.packer_ch, friction_brake_bus, self.apply_brake,
