3a00a78769f0eee9eeeba69c09d32be02904fb35
SangJo Kim <kandrea@nate.com>
2025-06-13 08:59:52 +0900

    fix

--- opendbc_repo/opendbc/safety/safety/safety_gm.h
+++ opendbc_repo/opendbc/safety/safety/safety_gm.h
@@ -53,8 +53,9 @@ static void gm_rx_hook(const CANPacket_t *to_push) {
     if ((addr == 0x1E1) && ((gm_hw == GM_ASCM) || !gm_pcm_cruise || gm_cc_long || gm_cam_long)) {
       int button = (GET_BYTE(to_push, 5) & 0x70U) >> 4;
 
-      // enter controls on falling edge of set or rising edge of resume (avoids fault)
+      // fall edge: 누르고 있다가 떼는 것에서 리쥼버튼처럼 rising edge(이전에 안눌렸던 것을 지금 누르는 것)으로 SET조건 변경
       bool set = (button == GM_BTN_SET) && (cruise_button_prev != GM_BTN_SET);
+      // 리쥼은 기존처럼 falling edge 유지
       bool res = (button == GM_BTN_RESUME) && (cruise_button_prev != GM_BTN_RESUME);
       if (set || res) {
         controls_allowed = true;
@@ -200,9 +201,9 @@ static bool gm_tx_hook(const CANPacket_t *to_send) {
   // BUTTONS: used for resume spamming and cruise cancellation with stock longitudinal
   if (addr == 0x1E1) {
     int button = (GET_BYTE(to_send, 5) >> 4) & 0x7U;
-
+    // 크루즈 선행조건 없이 CANCEL/SET/RESUME 진입 허용
     bool allowed_btn = (button == GM_BTN_SET) || (button == GM_BTN_RESUME) || (button == GM_BTN_CANCEL);
-
+    // 모두 허용후 차량별로 나머지 버튼허용
     if ((gm_hw == GM_ASCM) || gm_cam_long) {
       // OP 롱컨 + CAM_LONG 차량
       allowed_btn |= (button == GM_BTN_UNPRESS);
