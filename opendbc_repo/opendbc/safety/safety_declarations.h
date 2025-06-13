d02b696509e4dc8b0c8952c42f46e25c5ccc2732
SangJo Kim <kandrea@nate.com>
2025-06-14 08:07:18 +0900

    fix

--- opendbc_repo/opendbc/safety/safety_declarations.h
+++ opendbc_repo/opendbc/safety/safety_declarations.h
@@ -216,6 +216,7 @@ extern bool acc_main_on; // referred to as "ACC off" in ISO 15622:2018
 extern int cruise_button_prev;
 extern int cruise_main_prev;
 extern bool safety_rx_checks_invalid;
+extern bool aol_allowed;
 
 // for safety modes with torque steering control
 extern int desired_torque_last;       // last desired steer torque
