f5b80f3517632e09fed969f7c6a823e7d7e322e3
SangJo Kim <kandrea@nate.com>
2025-06-20 19:30:29 +0900

    finish

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -13,18 +13,12 @@ LongCtrlState = car.CarControl.Actuators.LongControlState
 
 def long_control_state_trans(CP, active, long_control_state, v_ego,
                              should_stop, brake_pressed, cruise_standstill, a_ego, stopping_accel, radarState):
-  stop_dist = 10.0 # 미터단위
+  stop_dist = 20.0 # 미터단위
   lead = radarState.leadOne
   stopping_condition = should_stop or (lead.status and lead.dRel < stop_dist)
   # 오픈파일럿이 LongitudinalControl 중일 때만, 멈춘 뒤 브레이크 해제 무시
   if CP.openpilotLongitudinalControl and long_control_state == LongCtrlState.stopping:
     brake_pressed = True
-  #dRel: 앞차 거리, should_stop: MPC 기준, stopping_condition: 최종 정지 진입 조건
-  print(f"[STOP_DBG] dRel={lead.dRel:.2f}m, "
-        f"should_stop={should_stop}, "
-        f"stop_dist={stop_dist}, "
-        f"stopping_condition={stopping_condition}, "
-        f"prev_state={long_control_state}")
   starting_condition = (not should_stop and
                         not cruise_standstill and
                         not brake_pressed)
