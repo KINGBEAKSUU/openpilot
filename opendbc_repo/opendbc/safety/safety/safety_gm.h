3106b309d3841595044f9f82ee17025fab63c420
SangJo Kim <kandrea@nate.com>
2025-06-20 12:23:40 +0900

    revert cruise_engaged_prev

--- opendbc_repo/opendbc/safety/safety/safety_gm.h
+++ opendbc_repo/opendbc/safety/safety/safety_gm.h
@@ -232,7 +232,7 @@ static bool gm_tx_hook(const CANPacket_t *to_send) {
     bool allowed_btn = (button == GM_BTN_CANCEL) && cruise_engaged_prev;
     // For standard CC, allow spamming of SET / RESUME
     if ((gm_hw == GM_ASCM) || (gm_hw == GM_CAM) || gm_cam_long || gm_cc_long) {
-      allowed_btn |= cruise_engaged_prev && ((button == GM_BTN_SET) || (button == GM_BTN_RESUME) || (button == GM_BTN_UNPRESS));
+      allowed_btn |= ((button == GM_BTN_SET) || (button == GM_BTN_RESUME) || (button == GM_BTN_UNPRESS));
     }
 
     if (!allowed_btn) {
