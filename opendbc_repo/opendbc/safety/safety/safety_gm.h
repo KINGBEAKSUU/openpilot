7c70dd9d78546193939b0d5b23d449574d12cfa1
SangJo Kim <kandrea@nate.com>
2025-06-29 17:21:50 +0900

    fix gas

--- opendbc_repo/opendbc/safety/safety/safety_gm.h
+++ opendbc_repo/opendbc/safety/safety/safety_gm.h
@@ -218,12 +218,7 @@ static bool gm_tx_hook(const CANPacket_t *to_send) {
       if(!controls_allowed) print("@@auto cruise control enabled....\n");
         controls_allowed = true;        
     }
-    int gas_regen = 0;
-    if ((gm_hw == GM_ASCM) || gm_cam_long) {
-      gas_regen = ((GET_BYTE(to_send, 2) & 0x7FU) << 5) + ((GET_BYTE(to_send, 3) & 0xF8U) >> 3);
-    } else if (gm_hw == GM_CAM) {
-      gas_regen = ((GET_BYTE(to_send, 1) & 0x1U) << 13) + ((GET_BYTE(to_send, 2) & 0xFFU) << 5) + ((GET_BYTE(to_send, 3) & 0xF8U) >> 3);
-    }
+    int gas_regen = ((GET_BYTE(to_send, 2) & 0x7FU) << 5) + ((GET_BYTE(to_send, 3) & 0xF8U) >> 3);
 
     bool violation = false;
     // Allow apply bit in pre-enabled and overriding states
