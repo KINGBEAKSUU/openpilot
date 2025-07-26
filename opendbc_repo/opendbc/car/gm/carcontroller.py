05a8df8740a61538c062df1f10522439942dcfb6
SangJo Kim <kandrea@nate.com>
2025-07-26 13:46:02 +0900

    fix auto Cruise(remove UNPRESS)

--- opendbc_repo/opendbc/car/gm/carcontroller.py
+++ opendbc_repo/opendbc/car/gm/carcontroller.py
@@ -169,14 +169,14 @@ class CarController(CarControllerBase):
           if (self.frame - self.last_button_frame) * DT_CTRL > 0.04:
             self.last_button_frame = self.frame
             can_sends.append(gmcan.create_buttons(self.packer_pt, CanBus.POWERTRAIN, button_counter, CruiseButtons.DECEL_SET))
-            self.waiting_for_release = True
+            #self.waiting_for_release = True
 
         # Release (DECEL_SET → UNPRESS)
-        elif self.waiting_for_release and (self.frame - self.last_button_frame) * DT_CTRL > 0.04:
+        """elif self.waiting_for_release and (self.frame - self.last_button_frame) * DT_CTRL > 0.04:
           if CS.out.activateCruise and not CS.out.cruiseState.enabled:
             self.last_button_frame = self.frame
             can_sends.append(gmcan.create_buttons(self.packer_pt, CanBus.POWERTRAIN, button_counter, CruiseButtons.UNPRESS))
-            self.waiting_for_release = False
+            self.waiting_for_release = False"""
 
         # CamAcc: AutoResume 1st step(CANCEL버튼 전송 = 브레이크 토글용)
         elif actuators.longControlState == LongCtrlState.starting:
