5d1eb1aea438b5348f0c4652ebe7ee19382334c8
SangJo Kim <kandrea@nate.com>
2025-07-27 13:13:12 +0900

    fix

--- opendbc_repo/opendbc/safety/safety/safety_gm.h
+++ opendbc_repo/opendbc/safety/safety/safety_gm.h
@@ -150,7 +150,7 @@ static void gm_rx_hook(const CANPacket_t *to_push) {
     bool stock_ecu_detected = (addr == 0x180);  // ASCMLKASteeringCmd
 
     // Check ASCMGasRegenCmd only if we're blocking it
-    // Kans: gm_cam_long, gm_cc_long 차량은 순정 0x2CB 무시.
+    // Kans: gm_cam_long 차량은 순정 0x2CB 무시.
     if (!gm_pcm_cruise && !gm_pedal_long && (addr == 0x2CB) && !gm_cam_long) {
       stock_ecu_detected = true;
     }
