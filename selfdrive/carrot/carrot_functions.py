4ad7ba92955cdb601cbc44eb494c43ff4a1b674a
SangJo Kim <kandrea@nate.com>
2025-09-03 18:52:03 +0900

    fix trafficStopDist

--- selfdrive/carrot/carrot_functions.py
+++ selfdrive/carrot/carrot_functions.py
@@ -79,7 +79,7 @@ class CarrotPlanner:
     self.stopSignCount = 0
 
     self.stop_distance = 6.0
-    self.trafficStopDistanceAdjust = 1.8 #params.get_float("TrafficStopDistanceAdjust") / 100.
+    self.trafficStopDistanceAdjust = 0.3 #params.get_float("TrafficStopDistanceAdjust") / 100.
     self.comfortBrake = 2.4
     self.comfort_brake = self.comfortBrake
 
@@ -241,11 +241,12 @@ class CarrotPlanner:
 
     if v_ego_kph < 1.0:
       stopSign = model_x < 20.0 and model_v < 10.0
+    margin = float(np.interp(v_ego_kph, [0, 20, 40, 60, 82], [2.0, 3.0, 4.0, 5.0, 5.5]))
     elif v_ego_kph < 82.0:
-      stopSign = (model_x < d_rel - 2.0 and
+      stopSign = (model_x < d_rel - margin and
                   model_x < np.interp(v[0] * 3.6, [60, 80], [90.0, 110]) and
-                  ((model_v < 2.0) or (model_v < v[0] * 0.5)) and
-                  abs(y[-1]) < 6.0)
+                  ((model_v < 2.5) or (model_v < v[0] * 0.5)) and
+                  abs(y[-1]) < 5.0)
       # 정상주행중 감속하는 경우(카메라 감속등), 오감지가 많음. 
       # 회생감속시:v_cruise=0에는 신호호감지하도록함.
       if v_cruise != 0 and (self.xState == XState.e2eCruise and a_ego < -1.0):
