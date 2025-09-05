78ec115e098683c0de4ddfcac3513c1ca094bcce
SangJo Kim <kandrea@nate.com>
2025-09-05 16:31:10 +0900

    fix float->int

--- opendbc_repo/opendbc/car/gm/carcontroller.py
+++ opendbc_repo/opendbc/car/gm/carcontroller.py
@@ -400,10 +400,10 @@ class CarController(CarControllerBase):
     # RollinfCounter초기화
     if bus == CanBus.CAMERA:
       if self.btn_rc_cam < 0:
-        self.btn_rc_cam = CS.buttons_counter & 0x3
+        self.btn_rc_cam = int(CS.buttons_counter) & 0x3
     elif bus == CanBus.POWERTRAIN:
       if self.btn_rc_pt < 0:
-        self.btn_rc_pt = CS.buttons_counter & 0x3
+        self.btn_rc_pt = int(CS.buttons_counter) & 0x3
     # RollinfCounter +1증가
     if bus == CanBus.CAMERA:
       self.btn_rc_cam = (self.btn_rc_cam + 1) & 0x3
