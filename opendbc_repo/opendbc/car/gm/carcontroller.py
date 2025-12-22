8ae183fa688422685ab5f70d725f0374e321184a
kan-sky <kandrea@nate.com>
2025-12-22 21:08:55 +0900

    fix 1E1(SDGM) 0 bus

--- opendbc_repo/opendbc/car/gm/carcontroller.py
+++ opendbc_repo/opendbc/car/gm/carcontroller.py
@@ -555,7 +555,7 @@ class CarController(CarControllerBase):
   def send_btn(self, CS, can_sends, cruise_btn, bus=None):
     if bus is None:
       if self.CP.carFingerprint in SDGM_CAR:
-        bus = CanBus.CAMERA
+        bus = CanBus.POWERTRAIN
       #elif self.CP.networkLocation == NetworkLocation.fwdCamera and self.CP.carFingerprint not in CC_ONLY_CAR:
       #  bus = CanBus.POWERTRAIN
       else:
