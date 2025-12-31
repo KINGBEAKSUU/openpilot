8eb356ffa617ab67650e05827e17757237c79b10
kan-sky <kandrea@nate.com>
2025-12-31 21:17:14 +0900

    fix Max Brake(400)

--- opendbc_repo/opendbc/car/gm/gmcan.py
+++ opendbc_repo/opendbc/car/gm/gmcan.py
@@ -125,7 +125,8 @@ def create_friction_brake_command(packer, bus, apply_brake, idx, enabled, near_s
     #  mode = 0xB
 
 
-  apply_brake = max(0, min(0x7FF, int(apply_brake)))
+  #apply_brake = max(0, min(0x7FF, int(apply_brake)))
+  apply_brake = max(0, min(400, int(apply_brake)))
   brake = (0x1000 - apply_brake) & 0xFFF
   rc = int(idx) & 0x3  # 2비트 롤링카운터
   checksum = (0x10000 - (mode << 12) - brake - rc) & 0xFFFF
